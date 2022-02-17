15bfe749d0303e7a963631a41060031ad3cfc488
RafaelC-99 <rafaelcardosocatuti@gmail.com>
2022-02-17 15:45:57 -0300

    update 2

---
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation.personapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+//import org.springframework.web.bind.annotation.RequestBody;
+
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPeson = personRepository.save(person);
+        return MessageResponseDTO.builder().message("Created person with ID " + savedPeson.getId()).build();
+    }
+}
